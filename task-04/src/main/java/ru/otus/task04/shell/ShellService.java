2657b33a04572ba515e6d38e266a5f588fbda2d6
NataliaBuryakova <qqqq10q@gmail.com>
2021-03-05 09:02:47 +0400

    Task-04 ИСправлен NPE при вызове команды start  без команды login

--- task-04/src/main/java/ru/otus/task04/shell/ShellService.java
+++ task-04/src/main/java/ru/otus/task04/shell/ShellService.java
@@ -1,8 +1,10 @@
 package ru.otus.task04.shell;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Availability;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellMethodAvailability;
 import org.springframework.shell.standard.ShellOption;
 import ru.otus.task04.domain.Person;
 import ru.otus.task04.service.TestDialogueService;
@@ -22,9 +24,14 @@ public class ShellService {
         this.person = testDialogueService.login(userName);
     }
     @ShellMethod(key = "start",value ="start test")
+    @ShellMethodAvailability({"checkLogin"})
     public void startApp(){
         testDialogueService.startTestFromPerson(person);
     }
 
+    public Availability checkLogin() {
+        String message = "Sorry! You are not login";
+        return person!=null? Availability.available() : Availability.unavailable(message);
+    }
 
 }
